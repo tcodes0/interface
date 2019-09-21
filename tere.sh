553106d6a9cb76a480150733cabceb5eb9619370
Raphael Thomazella <thomazella9@gmail.com>
2019-09-21 15:42:23 -0300

    chore(misc): cleanup old scripts

--- tere.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [[ "$#" == 0 ]] || [ "$1" == '-h' ]; then
-  echo provide two emoticons to make a ❤️   for her.
-  exit
-fi
-model="🕞🕞🕞🕞🕞🕞🕞🕞🕞🕞🕞\n🕞🕛🕛🕛🕞🕞🕞🕛🕛🕛🕞\n🕛🕛🕛🕛🕛🕞🕛🕛🕛🕛🕛\n🕛🕛🕛🕛🕛🕛🕛🕛🕛🕛🕛\n🕞🕛🕛🕛🕛🕛🕛🕛🕛🕛🕞\n🕞🕞🕛🕛🕛🕛🕛🕛🕛🕞🕞\n🕞🕞🕞🕛🕛🕛🕛🕛🕞🕞🕞\n🕞🕞🕞🕞🕛🕛🕛🕞🕞🕞🕞\n🕞🕞🕞🕞🕞🕛🕞🕞🕞🕞🕞\n"
-model=$(gsed -Ee s/🕞/"$1"/gm <<< "$model")
-model=$(gsed -Ee s/🕛/"$2"/gm <<< "$model")
-echo -e $model | pbcopy
