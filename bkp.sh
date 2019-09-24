224092bcb6b9a16c7fb1658b1af94f1784c97510
Raphael Thomazella <thomazella9@gmail.com>
2019-09-24 09:23:43 -0300

    chore(misc): remobe old scripts failing bkp.sh

--- bkp.sh
+++ bkp.sh
@@ -1,19 +1,19 @@
 #! /usr/bin/env bash
 # shellcheck disable=SC2154
 ##-----------------------  Deps & Setup  ----------------------##
-for name in bkp-routines optar progress paralol; do
-    # shellcheck disable=SC1090
-    source "$HOME/bin/$name.sh" || bailout "Dependency $name failed"
+for name in bkp-routines progress; do
+  # shellcheck disable=SC1090
+  source "$HOME/bin/$name.sh" || bailout "Dependency $name failed"
 done
 
 parse-options "$@"
 
 ######----------------- Quick exits  -----------------######
 if [[ "$#" != 0 ]]; then
-    if [ "$help" ] || [ "$h" ]; then
-        do-help
-        exit 1
-    fi
+  if [ "$help" ] || [ "$h" ]; then
+    do-help
+    exit 1
+  fi
 fi
 
 #-- Test for backup drive plugged in
