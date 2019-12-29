7bd6708cabfe56eb1b50f4cccd0859b64ed9fc62
Raphael Thomazella <thomazella9@gmail.com>
2019-12-29 11:44:25 -0300

    chore(misc): remove --LINE-NUMBERS from LESS env var

--- .bashrc.bash
+++ .bashrc.bash
@@ -23,7 +23,7 @@ export HISTTIMEFORMAT="%b %d "
 export HISTCONTROL="ignoredups:erasedups"
 export TIMEFORMAT=$'\n-time elapsed-\nreal\t%3Rs\nuser\t%3Us\nsystem\t%3Ss'
 export BLOCKSIZE=1000000 #1 Megabyte
-export LESS="--RAW-CONTROL-CHARS --HILITE-UNREAD --window=-5 --quiet --LINE-NUMBERS --buffers=32768 --quit-if-one-screen --prompt=?eEND:%pb\\%. ?f%F:Stdin.\\: page %d of %D, line %lb of %L"
+export LESS="--RAW-CONTROL-CHARS --HILITE-UNREAD --window=-5 --quiet --buffers=32768 --quit-if-one-screen --prompt=?eEND:%pb\\%. ?f%F:Stdin.\\: page %d of %D, line %lb of %L"
 export PAGER="less"
 export BASH_ENV="$HOME/.bashrc"
 
