898079d181e5b1b33368b26c64a4afcd923b0bd4
Raphael Thomazella <thomazella9@gmail.com>
2020-04-22 15:51:38 -0300

    feat(notify): add

--- .functions.bash
+++ .functions.bash
@@ -1117,3 +1117,28 @@ se() {
   oathtool -b --totp "$(cat "$FILE")" | pbcopy
   pbpaste
 }
+
+notify() {
+  if [ "$1" == '-h' ] || [ "$1" == '--help' ]; then
+    echo "
+    Send system notifications using MacOs osascript
+    notify <title?> <content?>
+    "
+    return
+  fi
+  if ! command -v osascript >/dev/null; then
+    echo "osascript not found on \$PATH. You running this on MacOs right?"
+    return 1
+  fi
+   title="Hello World"
+   text="Example notification. Use -h for help"
+  if [ "$1" ]; then
+    title="$1"
+  fi
+  if [ "$2" ]; then
+    text="$2"
+  fi
+  osascript -e "
+    display notification \"$text\" with title \"$title\"
+  "
+}
