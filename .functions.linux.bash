3d3e8517b51430e7b62ddc333cc6b8a057b17de4
Raphael Thomazella <thomazella9@gmail.com>
2020-07-21 01:13:45 -0300

    feat(chpwd): add trivia

--- .functions.linux.bash
+++ .functions.linux.bash
@@ -14,32 +14,11 @@ goo() {
 
 chpwd() {
   case $PWD in
-  $HOME/Desktop/community)
-    source "$HOME/Desktop/interface/chpwd-to-source/community/on-enter-dir"
-    ;;
-  $HOME/Desktop/confy)
-    source "$HOME/Desktop/interface/chpwd-to-source/confy/on-enter-dir"
-    ;;
-  $HOME/Desktop/elixir-backend-example)
-    source "$HOME/Desktop/interface/chpwd-to-source/elixir-backend-example/on-enter-dir"
+  $HOME/Desktop/trivia)
+    source "$HOME/Desktop/interface/chpwd-to-source/trivia/on-enter-dir"
     ;;
   $HOME/Desktop/interface)
     source "$HOME/Desktop/interface/chpwd-to-source/interface/on-enter-dir"
-    ;;
-  $HOME/Desktop/oreid)
-    source "$HOME/Desktop/interface/chpwd-to-source/oreid/on-enter-dir"
-    ;;
-  $HOME/Desktop/procure)
-    source ".env"
-    ;;
-  $HOME/Desktop/sense)
-    source "$HOME/Desktop/interface/chpwd-to-source/sense/on-enter-dir"
-    ;;
-  $HOME/Desktop/taffy)
-    source "$HOME/Desktop/interface/chpwd-to-source/taffy/on-enter-dir"
-    ;;
-  $HOME/Desktop/another-elixir)
-    source "$HOME/Desktop/interface/chpwd-to-source/another-elixir/on-enter-dir"
     ;;
   $HOME/Desktop/helpers-console)
     source "$HOME/Desktop/interface/chpwd-to-source/helpers-console/on-enter-dir"
@@ -47,48 +26,21 @@ chpwd() {
   $HOME/Desktop/efis)
     source "$HOME/Desktop/interface/chpwd-to-source/efis/on-enter-dir"
     ;;
-  $HOME/Desktop/acert)
-    source "$HOME/Desktop/interface/chpwd-to-source/acert/on-enter-dir"
-    ;;
   *) ;;
   esac
 
   case $OLDPWD in
-  $HOME/Desktop/community)
-    source "$HOME/Desktop/interface/chpwd-to-source/community/on-leave-dir"
-    ;;
-  $HOME/Desktop/confy)
-    source "$HOME/Desktop/interface/chpwd-to-source/confy/on-leave-dir"
-    ;;
-  $HOME/Desktop/elixir-backend-example)
-    source "$HOME/Desktop/interface/chpwd-to-source/elixir-backend-example/on-leave-dir"
+  $HOME/Desktop/trivia)
+    source "$HOME/Desktop/interface/chpwd-to-source/trivia/on-leave-dir"
     ;;
   $HOME/Desktop/interface)
     source "$HOME/Desktop/interface/chpwd-to-source/interface/on-leave-dir"
-    ;;
-  $HOME/Desktop/oreid)
-    source "$HOME/Desktop/interface/chpwd-to-source/oreid/on-leave-dir"
-    ;;
-  $HOME/Desktop/procure)
-    source "$HOME/Desktop/interface/chpwd-to-source/procure/on-leave-dir"
-    ;;
-  $HOME/Desktop/sense)
-    source "$HOME/Desktop/interface/chpwd-to-source/sense/on-leave-dir"
-    ;;
-  $HOME/Desktop/taffy)
-    source "$HOME/Desktop/interface/chpwd-to-source/taffy/on-leave-dir"
-    ;;
-  $HOME/Desktop/another-elixir)
-    source "$HOME/Desktop/interface/chpwd-to-source/another-elixir/on-leave-dir"
     ;;
   $HOME/Desktop/helpers-console)
     source "$HOME/Desktop/interface/chpwd-to-source/helpers-console/on-leave-dir"
     ;;
   $HOME/Desktop/efis)
     source "$HOME/Desktop/interface/chpwd-to-source/efis/on-leave-dir"
-    ;;
-  $HOME/Desktop/acert)
-    source "$HOME/Desktop/interface/chpwd-to-source/acert/on-leave-dir"
     ;;
   *) ;;
   esac
