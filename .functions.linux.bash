0066332f5d91a875c6e7173c20acff09f6d55b11
Raphael Thomazella <thomazella9@gmail.com>
2020-08-27 01:19:33 -0300

    fix(pacu): correct flag

--- .functions.linux.bash
+++ .functions.linux.bash
@@ -155,8 +155,7 @@ pacu() {
   # pacman
   sudo pacman --sync --sysupgrade --refresh --noconfirm
   sudo pacman --sync --noconfirm --clean
-  # sudo pacman --sync --clean --noconfirm
-  yay --sync --sysupgrade --refresh --noconfirn
+  yay --sync --sysupgrade --refresh --noconfirm
   yay --sync --noconfirm --clean
   # misc
   mackup backup
