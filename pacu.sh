33f33bee30de89bcfe5e3dd7910687760ebc0a3e
Raphael Thomazella <thomazella9@gmail.com>
2021-02-01 23:32:28 -0300

    fix pacu

--- pacu.sh
+++ pacu.sh
@@ -6,13 +6,17 @@ errExit() {
   exit 1
 }
 
+asdf_() {
+  /home/vacation/.asdf/bin/asdf "$@"
+}
+
 if ! yay --sync --sysupgrade --refresh --noconfirm; then errExit yay --sync failed; fi
 if ! yay --sync --noconfirm --clean; then errExit yay --clean; fi
-if ! mackup backup; then errExit mackup failed; fi
+# if ! mackup backup; then errExit mackup failed; fi
 if ! yarn global upgrade --latest; then errExit yarn global update failed; fi
 if ! yarn cache clean; then errExit yarn cache clean failed; fi
 if ! command cd "$NVM_DIR" && hub pull; then errExit nvm pull failed; fi
 if ! nvm install node; then errExit nvm install node failed; fi
-if ! asdf update; then errExit asdf update failed; fi
-if ! asdf plugin update --all; then errExit asdf plugin update failed; fi
+if ! asdf_ update; then errExit asdf_ update failed; fi
+if ! asdf_ plugin update --all; then errExit asdf_ plugin update failed; fi
 command cd "$HOME/Desktop"
