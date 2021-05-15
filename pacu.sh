3f0b9224a0670cd8320e2cf3bc4fb306fb7d6868
Raphael Thomazella <thomazella9@gmail.com>
2021-05-15 19:33:53 -0300

    updates

--- pacu.sh
+++ pacu.sh
@@ -10,7 +10,7 @@ asdf_() {
   /home/vacation/.asdf/bin/asdf "$@"
 }
 
-if ! yay --sync --sysupgrade --refresh; then errExit yay --sync failed; fi
+if ! yay --sync --sysupgrade --refresh --refresh; then errExit yay --sync failed; fi
 if ! yay --sync --clean; then errExit yay --clean; fi
 if ! mackup backup; then errExit mackup failed; fi
 if ! yarn global upgrade --latest; then errExit yarn global update failed; fi
@@ -19,4 +19,4 @@ if ! command cd "$NVM_DIR" && hub pull; then errExit nvm pull failed; fi
 if ! nvm install node; then errExit nvm install node failed; fi
 if ! asdf_ update; then errExit asdf_ update failed; fi
 if ! asdf_ plugin update --all; then errExit asdf_ plugin update failed; fi
-command cd "$HOME/Desktop"
+command cd "$HOME/Desktop" || exit 1
