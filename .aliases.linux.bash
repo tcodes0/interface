c84efa7e7d6cd33c08438aaf70827deebcbcf15a
Raphael Thomazella <thomazella9@gmail.com>
2021-05-28 18:04:05 -0300

    consolidate pac and yay aliases

--- .aliases.linux.bash
+++ .aliases.linux.bash
@@ -44,46 +44,29 @@ alias sysurestart='systemctl --user restart'
 alias sysustart='systemctl --user start'
 alias sysustop='systemctl --user stop'
 alias sysuenab='systemctl --user enable'
-########
-# pacman
-########
+#################
+# pacman and yay
+#################
 alias pac='pacman'
-alias pacInfo='pacman --sync --info'                          # -Si
-alias pacInfoVerbose='pacman --sync --info --info'            # -Sii
-alias pacList='pacman --query'                                # -Q
-alias pacListVerbose='pacman --query --list'                  # -Ql
-alias pacInstall='sudo pacman --sync --refresh --noconfirm'   # -S
-alias pacRemove='sudo pacman --remove'                        # -R
-alias pacRemoveForce='sudo pacman --remove --nodeps --nodeps' # -Rdd
+alias pacSearch='yay --sync --search' # -Ss
 alias pacWhyFile='pacman -Qo'
-alias paci='pacInfo'
-alias pacii='pacInfoVerbose'
-alias pacl='pacList'
-alias pacql='pacListVerbose'
-alias pacI='pacInstall'
-alias pacR='pacRemove'
-alias pacRdd='pacRemoveForce'
-alias pacWhyFile='pacman -Qo'
-#####
-# yay
-#####
-# search AUR too
-alias pacSearch='yay --sync --search'                 # -Ss
-alias yayInfo='yay --sync --info'                     # -Si
-alias yayInfoVerbose='yay --sync --info --info'       # -Sii
-alias yayList='yay --query'                           # -Q
-alias yayListVerbose='yay --query --list'             # -Ql
-alias yayInstall='yay --sync --refresh --noconfirm'   # -S
-alias yayRemove='yay --remove'                        # -R
-alias yayRemoveForce='yay --remove --nodeps --nodeps' # -Rdd
 alias pacs='pacSearch'
-alias yayi='yayInfo'
-alias yayii='yayInfoVerbose'
-alias yayl='yayList'
-alias yayql='yayListVerbose'
-alias yayI='yayInstall'
-alias yayR='yayRemove'
-alias yayRdd='yayRemoveForce'
+alias pacInfo='yay --sync --info'                          # -Si
+alias pacInfoVerbose='yay --sync --info --info'            # -Sii
+alias pacList='yay --query'                                # -Q
+alias pacListVerbose='yay --query --list'                  # -Ql
+alias pacInstall='yay --sync --refresh --noconfirm'        # -S
+alias pacRemove='yay --remove'                             # -R
+alias pacRemoveDeps='yay --remove --recursive --recursive' # -Rss
+alias pacRemoveForce='yay --remove --nodeps --nodeps'      # -Rdd
+alias paci='yayInfo'
+alias pacii='yayInfoVerbose'
+alias pacl='yayList'
+alias pacql='yayListVerbose'
+alias pacI='yayInstall'
+alias pacR='yayRemove'
+alias pacRss='yayRemoveDeps'
+alias pacRdd='yayRemoveForce'
 ########
 # others
 ########
