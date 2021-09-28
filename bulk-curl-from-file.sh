887e9482a159d68ec71deb5c6f3251e91a4c918f
Raphael Thomazella <thomazella9@gmail.com>
2021-09-28 00:08:59 -0300

    bulk curl form file

---
+++ bulk-curl-from-file.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+do-help() {
+  echo "\
+  Usage
+    bulk-curl-from-file.sh <file-path>
+      read lines from file and pass them as args to command
+      file-path IS RELATIVE
+  "
+}
+
+if [ "$#" -lt 1 ] || [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+  do-help
+  exit 1
+fi
+
+file=$1
+filePath=${PWD}/$file
+
+if [ ! -f "$filePath" ]; then
+  do-help
+  echo "File $filePath not found\\n"
+  exit 1
+fi
+
+setName=${file/.txt/}
+i=1
+
+while read -r line; do
+  eval "echo curl '$line' -L -o '${setName}-${i}.jpg'"
+  i=$((i+1))
+done <"$filePath"
+
+exit $?
