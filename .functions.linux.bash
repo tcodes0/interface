207c03f0b5c7b0e6622243a04a77080466548347
Thom Ribeiro <thomazella9@gmail.com>
2022-04-04 12:14:56 -0400

    chore(misc): add tableinfo

--- .functions.linux.bash
+++ .functions.linux.bash
@@ -217,3 +217,13 @@ mkinitcpio() {
 }
 
 #----------------
+
+tableinfo() {
+  if [ "$1" == "" ]; then
+    echo -n "\"SELECT column_name, is_nullable, data_type, column_default FROM information_schema.columns WHERE table_name = 'foo';\"" | xclip -selection c
+    echo "copied to clipboard as foo"
+  else
+    echo -n "\"SELECT column_name, is_nullable, data_type, column_default FROM information_schema.columns WHERE table_name = '$1';\"" | xclip -selection c
+    echo "copied to clipboard as $1"
+  fi
+}
