0427734b6372bf08ef6827b2401ef7658ffd876d
Thom Ribeiro <thomazella9@gmail.com>
2022-05-11 15:22:30 -0400

    tableinfo order

--- .functions.linux.bash
+++ .functions.linux.bash
@@ -220,10 +220,10 @@ mkinitcpio() {
 
 tableinfo() {
   if [ "$1" == "" ]; then
-    echo -n "\"SELECT column_name, is_nullable, data_type, column_default FROM information_schema.columns WHERE table_name = 'foo';\"" | xclip -selection c
+    echo -n "\"SELECT column_name, is_nullable, data_type, column_default FROM information_schema.columns WHERE table_name = 'foo' ORDER BY column_name;\"" | xclip -selection c
     echo "copied to clipboard as foo"
   else
-    echo -n "\"SELECT column_name, is_nullable, data_type, column_default FROM information_schema.columns WHERE table_name = '$1';\"" | xclip -selection c
+    echo -n "\"SELECT column_name, is_nullable, data_type, column_default FROM information_schema.columns WHERE table_name = '$1' ORDER BY column_name;\"" | xclip -selection c
     echo "copied to clipboard as $1"
   fi
 }
