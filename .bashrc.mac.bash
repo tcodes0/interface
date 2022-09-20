4543d75b28c36a545cb9305cf93122728f8d40a6
Thom Ribeiro <thomazella9@gmail.com>
2022-09-20 17:06:47 -0400

    google cloud env, keybindings

--- .bashrc.mac.bash
+++ .bashrc.mac.bash
@@ -18,6 +18,7 @@ safe_source $DOTFILE_PATH/.functions.bash
 safe_source $DOTFILE_PATH/.functions.mac.bash
 safe_source $DOTFILE_PATH/.prompt.linux.bash
 safe_source $DOTFILE_PATH/linux/home/.config/git-prompt.sh
+safe_source /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc
 
 #========== Environment
 
@@ -37,16 +38,8 @@ export PATH="\
 ${GOPATH}/bin:\
 ${GOROOT}/bin"
 
-# export MANPATH="/usr/local/opt/erlang/lib/erlang/man:$MANPATH"
-# export CDPATH=$HOME:/Volumes:$HOME/Desktop
 export EDITOR='code -w'
 export GOPATH="$HOME/.go"
-# LS_COLORS=$(cat "$HOME/Code/LS_COLORS/LS_COLORS_RAW")
-# export LS_COLORS
-
-# compiler flags
-# export LDFLAGS="-L/usr/local/opt/ruby/lib -L/usr/local/opt/libxslt/lib -L/usr/local/opt/openssl/lib -L/usr/local/opt/krb5/lib"
-# export CPPFLAGS="-I/usr/local/opt/ruby/include -I/usr/local/opt/libxslt/include -I/usr/local/opt/openssl/include -I/usr/local/opt/krb5/include"
 
 # NVM
 unset PREFIX            # NVM hates this
@@ -54,9 +47,3 @@ unset npm_config_prefix # NVM hates this
 export NVM_DIR="$HOME/.nvm"
 [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && source "/opt/homebrew/opt/nvm/nvm.sh"                   # This loads nvm
 [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && source "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
-
-# elixir/erlang with asdf
-# export ERL_AFLAGS="-kernel shell_history enabled"
-# safe_source /usr/local/opt/asdf/asdf.sh
-# export ERLANG_OPENSSL_PATH="/usr/local/opt/openssl"
-# export KERL_CONFIGURE_OPTIONS="--disable-debug --disable-silent-rules --without-javac --enable-shared-zlib --enable-dynamic-ssl-lib --enable-hipe --enable-sctp --enable-smp-support --enable-threads --enable-kernel-poll --enable-wx --enable-darwin-64bit --with-ssl=/usr/local/Cellar/openssl/1.0.2t"
