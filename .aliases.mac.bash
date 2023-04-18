5fdaefe7176d7084ac5c54118648de5791f1bd02
Thom Ribeiro <thomazella9@gmail.com>
2023-04-17 22:11:58 -0300

    rm gen func, bring some aliases over from main

--- .aliases.mac.bash
+++ .aliases.mac.bash
@@ -9,3 +9,23 @@
 
 alias ele-db-select="go run $HOME/Desktop/scripts/db-select/main.go"
 alias .i="cd \$DOTFILE_PATH"
+
+
+######
+# work
+######
+alias pdfret="echo return \&bytes.Buffer\{\}, nil"
+alias qhub="psql -U postgres -d hub -c"
+alias qhubtest="psql -U postgres -d hub_test -c"
+alias qmember="psql -U postgres -d member -c"
+alias qmembertest="psql -U postgres -d member_test -c"
+alias sqlqa="echo renamed to qahub and qamember"
+alias qahub="PGPASSWORD=\$(gcloud auth print-access-token) psql -U thom.ribeiro@eleanorhealth.com -d hub -h /home/vacation/.eleanor_sql_sockets/ele-qa-436057:us-east1:eleanor-postgres"
+alias qamember="PGPASSWORD=\$(gcloud auth print-access-token) psql -U thom.ribeiro@eleanorhealth.com -d member -h /home/vacation/.eleanor_sql_sockets/ele-qa-436057:us-east1:eleanor-postgres"
+alias kb="kubectl"
+alias deploy="make release ENV=prod"
+alias tableplus="LD_LIBRARY_PATH=/opt/tableplus/lib tableplus"
+alias golint="golangci-lint run --timeout 20s --tests=false"
+alias gen="godotenv -f .env go generate ./..."
+alias golint="golangci-lint run --timeout 20s --tests=false"
+alias ehvpn="gcloud alpha cloud-shell ssh --project=ele-qa-436057 --authorize-session -- -D 31337 -CNq; echo configure firefox to use SOCKS proxy v5 on port 31337"
--- .functions.bash
+++ .functions.bash
@@ -1079,13 +1079,3 @@ gcom() {
     gl
   fi
 }
-
-gen() {
-  if ! find . -type f -iregex '.*mock_.*go$' -execdir rm -f {} \;; then
-    echo "error running find regex exec rm on all mock* files"
-    return 1
-  fi
-
-  echo "generating mocks..."
-  godotenv -f .env go generate ./...
-}
