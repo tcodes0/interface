bc64fd57e20fcf13a9b86fdb6b15d21c8205e581
Thom Ribeiro <thomazella9@gmail.com>
2023-05-14 20:01:38 -0300

    improve chatgpt

--- .functions.bash
+++ .functions.bash
@@ -437,12 +437,12 @@ chatgpt() {
     return 1
   fi
 
-  curl -s https://api.openai.com/v1/chat/completions \
+  echo -en $(curl -s https://api.openai.com/v1/chat/completions \
     -H "Content-Type: application/json" \
     -H "Authorization: Bearer $OPENAI_API_KEY" \
     -d "{
     \"model\": \"gpt-3.5-turbo\",
     \"messages\": [{\"role\": \"user\", \"content\": \"$*\"}]
   }" |
-    jq '.choices[0].message.content'
+    jq '.choices[0].message.content')
 }
