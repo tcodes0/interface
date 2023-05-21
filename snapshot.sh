c45d44095adb92acd8ea3df998f84effb9e91de5
Thom Ribeiro <thomazella9@gmail.com>
2023-05-21 19:51:44 -0300

    snapshot.sh readonly option

--- snapshot.sh
+++ snapshot.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-response=""
-source=""
-destination=""
-today="$(date +%Y-%m-%d)"
-
 echo "subvolumes in /toplevel owned by you:"
 echo
 
@@ -14,6 +9,8 @@ find /toplevel -maxdepth 1 -user "$(whoami)" # will work for root also
 
 echo
 echo "snapshot source:"
+
+response=""
 read -r response
 
 # if response is not in toplevel, echo error and exit
@@ -23,15 +20,38 @@ if [ -z "$response" ] || [ ! -d "/toplevel/$response" ]; then
 fi
 
 source="$response"
+source_no_trailing="${response%%-*}"
 
-echo "snapshot name, enter to use $source-$today:"
+echo
+echo "read-only? (y/N) Default read-write:"
 read -r response
+
+readonly=""
+
+# if response is empty or n or N, set readonly to false
+if [ -n "$response" ] && [ "$response" != "n" ] && [ "$response" != "N" ]; then
+  readonly="-r"
+fi
+
+today="$(date +%Y-%m-%d)"
+default_name="$source_no_trailing-$today"
+
+#  if readonly is not empty, append -ro to default_name
+if [ -n "$readonly" ]; then
+  default_name="$default_name-ro"
+fi
+
+echo
+echo "snapshot name, default $default_name:"
+read -r response
+
+destination=""
 
 # if no response or response is not a string, echo error and exit
 if [ -z "$response" ]; then
-  destination="$source-$today"
+  destination="$default_name"
 else
   destination="$response"
 fi
 
-btrfs subvolume snapshot "/toplevel/$source" "/toplevel/$destination"
+btrfs subvolume snapshot $readonly "/toplevel/$source" "/toplevel/$destination"
