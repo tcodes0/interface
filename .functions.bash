6eacefeae344bbaed9ca0c3879bb4421cb99779c
Thom Ribeiro <thomazella9@gmail.com>
2023-07-04 18:54:39 -0300

    replace qai code with baristai

--- .functions.bash
+++ .functions.bash
@@ -434,22 +434,5 @@ __gen() {
 #- - - - - - - - - - -
 
 qai() {
-  if [ $# == "0" ]; then
-    echo "usage: qai <prompt>"
-    return 1
-  fi
-
-  if [ -z "$OPENAI_API_KEY" ]; then
-    echo "\$OPENAI_API_KEY not found in env"
-    return 1
-  fi
-
-  echo -en "$(curl -s https://api.openai.com/v1/chat/completions \
-    -H "Content-Type: application/json" \
-    -H "Authorization: Bearer $OPENAI_API_KEY" \
-    -d "{
-    \"model\": \"gpt-3.5-turbo\",
-    \"messages\": [{\"role\": \"user\", \"content\": \"$*\"}]
-  }" |
-    jq '.choices[0].message.content')"
+  baristai --use chatgpt --prompt "$*"
 }
