f33ff98faec77ab08c19836eb1061c5727827233
Thom Ribeiro <thomazella9@gmail.com>
2024-03-24 22:56:06 -0400

    update gen

--- .aliases.mac.bash
+++ .aliases.mac.bash
@@ -26,6 +26,6 @@ alias kb="kubectl"
 alias deploy="make release ENV=prod"
 alias tableplus="LD_LIBRARY_PATH=/opt/tableplus/lib tableplus"
 alias golint="golangci-lint run --timeout 20s --tests=false"
-alias gen="godotenv -f .env go generate ./..."
+alias gen='find . -name "mock_*" -exec rm -f {} \; && godotenv -f .env go generate ./...'
 alias golint="golangci-lint run --timeout 20s --tests=false"
 alias ehvpn="gcloud alpha cloud-shell ssh --project=ele-qa-436057 --authorize-session -- -D 31337 -CNq; echo configure firefox to use SOCKS proxy v5 on port 31337"
