08aec264f7c530fa1900d781a6ca18b0a87d83f3
Thom Ribeiro <thomazella9@gmail.com>
2024-04-28 10:26:23 -0300

    updates

--- .aliases.linux.bash
+++ .aliases.linux.bash
@@ -104,7 +104,7 @@ alias gen="godotenv -f .env go generate ./..."
 alias golint="golangci-lint run --timeout 20s --tests=false"
 alias ehvpn="gcloud alpha cloud-shell ssh --project=ele-qa-436057 --authorize-session -- -D 31337 -CNq; echo configure firefox to use SOCKS proxy v5 on port 31337"
 alias gorun="godotenv -f .env go run -race"
-alias gotest="godotenv -f .env go test ./... -race"
+alias gotest="godotenv -f .env go test ./... -race -json 2>&1 | gotestfmt"
 alias chromevpn='google-chrome-stable --proxy-server="socks5://localhost:31337"'
 alias ehtoken="gcloud auth print-access-token"
 alias kubecheck="kubeconform --strict --schema-location=https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master --schema-location=https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/{{.Group}}/{{.ResourceKind}}_{{.ResourceAPIVersion}}.json"
