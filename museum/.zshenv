0f5b9bbc8fc3b17ae54700650390a4caa881b296
Thom Ribeiro <thomazella9@gmail.com>
2024-05-31 02:30:25 -0300

    chore repo: cleanup old scirpts

--- museum/.zshenv
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env zsh
-
-if [ "$HOME/.ps1.zsh" ] && [[ "$(uname -s)" =~ Darwin ]]; then
-    source "$HOME/.ps1.zsh"
-fi
--- museum/subnautica.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-set -e
-
-cd "$HOME/SteamLibrary/steamapps/common/Subnautica/Subnautica.app/Contents/Resources/Data/Managed"
-# cd /Volumes/Izi/SteamLibrary/steamapps/common/Subnautica/Subnautica.app/Contents/Resources/Data/Managed
-mono QModManager.exe
