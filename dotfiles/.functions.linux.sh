9a68ac5b8e792f7be31622b3f2ebd37a1c2e10b2
Raphael "Thom" Thomazella <thomazella9@gmail.com>
2025-05-17 22:10:49 -0300

    feat(dotfiles): add sys function

--- dotfiles/.functions.linux.sh
+++ dotfiles/.functions.linux.sh
@@ -84,3 +84,78 @@ pac_orphan_deps_interactive() {
     yay --remove --recursive --recursive "$pkg"
   done
 }
+
+#- - - - - - - - - - -
+
+# todo: remove
+unalias sys 2>/dev/null
+unalias sysu 2>/dev/null
+
+# __sys: A smart wrapper for systemctl commands.
+# Supports 'system' (with sudo) and 'user' modes.
+# Allows fuzzy subcommand matching
+# Adds --now to 'enable' if user confirms.
+__sys() {
+  local type="$1" sub_cmd="$2" args="${*:3}" cmd sub_cmd args prefix="sys:"
+  local valid_abrev_sub_cmds=(status restart start stop enable daemon-reload cat)
+
+  if [[ "$type" == "system" ]]; then
+    sudo --validate || return 1
+    cmd="sudo systemctl"
+  elif [[ "$type" == "user" ]]; then
+    cmd="systemctl --user"
+  else
+    err "invalid first argument: must be 'system' or 'user'" >&2
+    return 1
+  fi
+
+  if [[ "$sub_cmd" == "" ]]; then
+    debug $LINENO "$cmd" "$args"
+    # shellcheck disable=SC2086
+    # on purpose, causes systemctl error if quoted
+    $cmd $args
+
+    return
+  fi
+
+  local matches=() answer
+
+  for option in "${valid_abrev_sub_cmds[@]}"; do
+    if [[ "$option" == "$sub_cmd"* ]]; then
+      matches+=("$option")
+    fi
+  done
+
+  if ((${#matches[@]} == 1)); then
+    sub_cmd="${matches[0]}"
+
+    if [[ "$sub_cmd" == "enable" ]]; then
+      read -r -p "Add --now to 'enable'? [y/N]" answer
+
+      if [[ "$answer" =~ ^[Yy]$ ]]; then
+        sub_cmd="enable --now"
+      fi
+    fi
+
+    debug $LINENO "$cmd" "$sub_cmd" "$args"
+    $cmd "$sub_cmd" "$args"
+  elif ((${#matches[@]} > 1)); then
+    msgln "$prefix ambiguous subcommand '$sub_cmd'. Possible matches:"
+    for match in "${matches[@]}"; do
+      msgln "\t$match"
+    done
+
+    return 1
+  else
+    debug $LINENO "$cmd" "$sub_cmd" "$args"
+    $cmd "$sub_cmd" "$args"
+  fi
+}
+
+sys() {
+  __sys "system" "$@"
+}
+
+sysu() {
+  __sys "user" "$@"
+}
